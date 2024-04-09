862870e453765b94210f91dbad3968755e5adab7
bdkautzman <briankautzman@gmail.com>
2024-04-09 15:43:42 -0500

    Add computer choice function

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// Randomly returns either 'Rock', 'Paper' or 'Scissors'
+function getComputerChoice() {
+
+}
+
+// Plays a single round of RPS, declares winner or tie
+function playRPS(playerSelection, computerSelection) {
+
+}
+
+// Plays a five round game of RPS, keeps score and declares
+// winner or loser at the end
+function playGame() {
+
+}

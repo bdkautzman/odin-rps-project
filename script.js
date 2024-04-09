b7bb583ad645c261d8e531f96d91c9b770935fd7
bdkautzman <briankautzman@gmail.com>
2024-04-09 16:09:31 -0500

    Add playRPS function, change computer choice function to return capitalized word

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 
 // Randomly returns either 'Rock', 'Paper' or 'Scissors'
 function getComputerChoice() {
+    let num = Math.floor((Math.random()*3));
 
+    switch(num) {
+        case 0:
+            return "rock";
+        
+        case 1:
+            return "paper";
+
+        case 2:
+            return "scissors";
+    }
 }
 
 // Plays a single round of RPS, declares winner or tie

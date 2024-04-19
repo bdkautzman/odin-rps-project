6c88c1122bd4e0dce90460525a3d7991aa26a0ba
bdkautzman <briankautzman@gmail.com>
2024-04-19 11:44:52 -0500

    Add UI

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ let pScore = 0, // Player score
     cScore = 0, // Computer score
     tScore = 0; // Ties
 
+const rockBtn = document.querySelector("#R");
+rockBtn.addEventListener("click", ()=> {
+    playGame("Rock");
+});
+
+const paperBtn = document.querySelector("#P");
+paperBtn.addEventListener("click", ()=> {
+    playGame("Paper");
+});
+
+const scissorsBtn = document.querySelector("#S");
+scissorsBtn.addEventListener("click", ()=> {
+    playGame("Scissors");
+});
+
+const pDiv = document.querySelector("#pScore");
+const cDiv = document.querySelector("#cScore");
+const tDiv = document.querySelector("#tScore");
+
 // Randomly returns either 'Rock', 'Paper' or 'Scissors'
 function getComputerChoice() {
     // Generate random 0, 1 or 3
@@ -31,28 +50,45 @@ function playRPS(playerSelection, computerSelection) {
     // Compare choices and return winner
     if (formattedString === computerSelection) {
         tScore++;
+        tDiv.textContent = "Tie score: " + tScore;
         return "Tie!";
     }
     else if ((formattedString === "Rock" && computerSelection === "Scissors") ||
             (formattedString === "Paper" && computerSelection === "Rock") || 
             (formattedString === "Scissors" && computerSelection === "Paper")) {
                 pScore++;
+                pDiv.textContent = "Player score: " + pScore;
+                if(pScore >= 5){
+                    endGame();
+                }
                 return "You win! " + formattedString + " beats " + computerSelection + "!";
     }
     else {
         cScore++;
+        cDiv.textContent = "Comupter score: " + cScore;
+        if(cScore >= 5){
+            endGame();
+        }
         return "You lose! " + computerSelection + " beats " + formattedString + "!"; 
     }  
 }
 
 // Plays a five round game of RPS, keeps score and declares
 // winner or loser at the end
-function playGame() {
-    for(let i = 0; i < 5; i++) {
-        let userChoice = prompt("What do you play? (rock, paper, scissors): ")
-        console.log(playRPS(userChoice, getComputerChoice()));
-    }
+function playGame(userChoice) {
+    playRPS(userChoice, getComputerChoice());
 }
 
-// Run the game!
-playGame();
+function endGame(){
+    if(pScore === 5){
+        alert("Player wins! Good game!");
+    }else{
+        alert("Computer wins! Better luck next time!");
+    }
+    pScore = 0;
+    cScore = 0;
+    tScore = 0;
+    pDiv.textContent = '';
+    cDiv.textContent = '';
+    tDiv.textContent = '';
+}
